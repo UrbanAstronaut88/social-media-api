cd09f5df758ef2a97ea84e8e420af322d01f9dca
BondarenkoKirill <bku089@gmail.com>
2025-08-06 18:41:15 +0300

    Added models to posts & users

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.email} - {self.created_at}"
