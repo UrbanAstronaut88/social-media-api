73eeb2031a46546686a0f5254410e42bf42667b5
BondarenkoKirill <bku089@gmail.com>
2025-08-08 15:23:25 +0300

    added admin settings

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from users.models import Follow
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "is_staff", "is_active")
+    search_fields = ("username", "email")
+    list_filter = ("is_staff", "is_active")
+    ordering = ("id",)
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("id", "follower", "following")
+    search_fields = ("follower__username", "following__username")
+    ordering = ("id",)
